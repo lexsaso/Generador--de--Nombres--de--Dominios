c740f3823c18baa019dd9b5f5361ed59bfbbc5c6
lexsaso <jalexanderssoto@hotmail.com>
2026-06-19 18:04:49 +0000

    generador-de-nom-dominios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+// 1. Definimos las listas de palabras (puedes añadir las que quieras)
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
+
+
+for (let i = 0; i < pronoun.length; i++) {
+
+  for (let j = 0; j < adj.length; j++) {
+
+    for (let k = 0; k < noun.length; k++) {
+
+      for (let l = 0; l < extensions.length; l++) {
+
+        let domainName = pronoun[i] + adj[j] + noun[k] + extensions[l];
+
+        console.log(domainName);
+      }
+    }
+  }
+}
